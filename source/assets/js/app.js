d92ff4eafcc8a37189f8a8c7ae207a47c0417ed9
Torus <pvne000@gmail.com>
2023-03-01 18:08:04 +0200

    Update Filters. Add AND filtering.

--- source/assets/js/app.js
+++ source/assets/js/app.js
@@ -6,23 +6,31 @@ require('bootstrap')
 $(function() {
     function badgeFilterOnClick() {
         let filterText = $(this).attr('data-title');
-        $('.js-on-click-filter-categories[title="' + filterText + '"]').removeClass('border-marked');
-        $('.js-on-click-filter-tags[title="' + filterText + '"]').removeClass('border-marked');
+        let filterGroup = $(this).attr('data-group');
+        $('.js-on-click-filter-' + filterGroup + '[title="' + filterText + '"]').removeClass('border-marked');
         $(this).remove();
+        if (listSetFilters[filterGroup].indexOf(filterText) !== -1) {
+            listSetFilters[filterGroup].splice(listSetFilters[filterGroup].indexOf(filterText), 1);
+        }
 
         renderCardSetFilter();
     }
 
     function renderAddFilter(el) {
         let filterText = $(el).attr('title');
+        let filterGroup = $(el).closest('div[data-filter-group]').attr('data-filter-group');
         if ($(el).hasClass('border-marked')) {
             $(el).removeClass('border-marked');
-            listFilters.find('.badge-filter[data-title="' + filterText + '"]').remove();
+            if (listSetFilters[filterGroup].indexOf(filterText) !== -1) {
+                listSetFilters[filterGroup].splice(listSetFilters[filterGroup].indexOf(filterText), 1);
+            }
+            listFilters.find('.badge-filter[data-group="' + filterGroup + '"][data-title="' + filterText + '"]').remove();
         } else {
             $(el).addClass('border-marked');
-            if (!listFilters.find('.badge-filter[data-title="' + filterText + '"]').length) {
-                listFilters.append(templateFilter.split('{text}').join(filterText));
-                $(document).on('click', '.badge-filter[data-title="' + filterText + '"]', badgeFilterOnClick);
+            if (!listFilters.find('.badge-filter[data-group="' + filterGroup + '"][data-title="' + filterText + '"]').length) {
+                listFilters.append(templateFilter.split('{text}').join(filterText).replace(/\{group\}/i, filterGroup));
+                listSetFilters[filterGroup].push(filterText);
+                $(document).on('click', '.badge-filter[data-group="' + filterGroup + '"][data-title="' + filterText + '"]', badgeFilterOnClick);
             }
         }
 
@@ -31,11 +39,25 @@ $(function() {
 
     function renderCardSetFilter() {
         let listCard = $(".js-card-filter");
-        if (listFilters.find(".badge-filter").length) {
+        let badgeFilter = listFilters.find(".badge-filter")
+        if (badgeFilter.length) {
             listCard.hide();
-            listFilters.find(".badge-filter").each(function () {
-                listCard.find("p.categories span:contains('" + $(this).attr('data-title') + "')").closest('.js-card-filter').show();
-                listCard.find("p.tags span:contains('" + $(this).attr('data-title') + "')").closest('.js-card-filter').show();
+            let stringFilters = {};
+            for (let key in listSetFilters) {
+                let str = listSetFilters[key].join("'), p." + key + " span:contains('");
+                if (str) {
+                    stringFilters[key] = '';
+                    stringFilters[key] = "p." + key + " span:contains('" + str +"')";
+                }
+            }
+            listCard.each(function () {
+                let foundFilterCount = 0;
+                for (var key in stringFilters) {
+                    if ($(this).find(stringFilters[key]).length) foundFilterCount++;
+                }
+                if (foundFilterCount === Object.keys(stringFilters).length) {
+                    $(this).show();
+                }
             });
         } else {
             listCard.show();
@@ -49,6 +71,10 @@ $(function() {
         listFilters.find(".badge-filter").removeClass('display-none');
         var templateFilter = listFilters.html();
         listFilters.find(".badge-filter").remove();
+        var listSetFilters = {};
+        $('div[data-filter-group]').each(function () {
+            listSetFilters[$(this).attr('data-filter-group')] = [];
+        });
 
         $(".js-on-click-filter-categories").click(function(e) {
             e.preventDefault();
